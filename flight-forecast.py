f164b7dbe88750729df09a6a73c0f25a2671cc8f
Alejandro Herrada <alejandropons17@gmail.com>
2018-05-07 18:32:32 -0400

    fixed graphing issues now graph is exported as png

--- flight-forecast.py
+++ flight-forecast.py
@@ -1,9 +1,8 @@
 import sys
 from lxml import html
 import requests
-from collections import Counter
 from matplotlib import pyplot as plt
-from datetime import datetime
+import numpy as np
 
 #use requests.get to retrieve the web page with our data, parse it using the html module and save the results in tree
 #command line arguments are used in the url so that start and end time can be specified when running the script
@@ -15,6 +14,22 @@ depart_times = tree.xpath('//*[@id=\"tableGrid\"]/tbody/tr/td[6]/text()')
 #use text_content and strip to remove unncecessary space character
 depart_gates = [td.text_content().strip() for td in tree.xpath('//*[@id=\"tableGrid\"]/tbody/tr/td[9]')]
 
+#this is the part whrere i give up and hardcode the list of elements that  I'm going to need to graph anything properly
+#morning hours, 5am to 1pm, value 5 to 13
+amlabels = ["5am", "6am", "7am", "8am", "9am", "10am", "11am", "12pm", "1pm"]
+amhours = range(5, 14)
+#afternoon hours, 1pm to 1am, values 13 to 25
+pmlabels = ["1pm", "2pm", "3pm", "4pm", "5pm", "6pm", "7pm", "8pm", "9pm", "10pm", "11pm", "12am", "1am"]
+pmhours = range(13, 26)
+
+#make assignments corresponding to time
+if sys.argv[1] == '300':
+    labels = amlabels
+    flights = dict.fromkeys(amhours, 0)
+else:
+    labels = pmlabels
+    flights = dict.fromkeys(pmhours, 0)
+
 #convert gate numbers from unicode string to int for easy use
 flight_gates = []
 for gate_number in depart_gates:
@@ -22,43 +37,27 @@ for gate_number in depart_gates:
     flight_gates.append(gate_number)
     
 #keep only the flight times for gates 100+, which is the terminal where i work
-#we only need the date and hour so we'll trim the minutes (format is ddd hh:mm) and format as hhxm
-flights = []
+#we only need the date and hour so we'll trim the minutes ddd hh:mm
+current_day = depart_times[0][:3]                   #store current day which should be the
+                                                    #first three letters of the time at the
+                                                    #begining of the list
+                                            
+#calculate and store the amount of flights by hour but only for gates 100+
 for i in range(len(flight_gates)):
     if flight_gates[i] > 99:
-        flight_hour = datetime.strptime(depart_times[i][5:7], "%H")
-        flight = flight_hour.strftime("%I%p")
-        flights.append(flight)
+        hour = int(depart_times[i][5:7])            #keep hour value of the time of flight, this will function as our key
+        if current_day == depart_times[i][:3]:      #check we're still on the same day otherwise shift hour value by 11
+            flights[hour] += 1
+        else:
+            flights[hour + 11] += 1
 
-#calculate and store the amount of flights by hour
-flightTally = Counter(flights)
-
-#TODO fix function so that it produces two separate lists, and use amhours and pmhours as labels instead
-#ugh
-def makeSchedule(counter, time):
-    """this is the moment where i give up and end up hardcoding the amount of 
-    hours in a day so that i can keep the relevant order when printing out the 
-    schedule and ensuring the line graph plots correctly"""
-    sortedschedule = []
-    
-    amhours = ["05AM", "06AM", "07AM", "08AM", "09AM", "10AM", "11AM", "12PM", "01PM"]
-    pmhours = ["01PM", "02PM", "03PM", "04PM", "05PM", "06PM", "07PM", "08PM", "09PM", "10PM", "11PM", "12AM", "01AM"]
-    
-    if time == "300":
-        for h in amhours:
-            sortedschedule.append((h, counter.get(h, 0)))
-    else:
-        for h in pmhours:
-            sortedschedule.append((h, counter.get(h, 0)))
-             
-    return sortedschedule
-
-schedule = makeSchedule(flightTally, sys.argv[1])
-print schedule
-
-#Graph the results4
-plt.plot(*zip(*schedule))
+#Graph the results
+plt.plot(flights.keys(), flights.values(), 'bo-')
 plt.xlabel("Time of flight")
 plt.ylabel("Number of flights")
 plt.title("Flights by hour")
-plt.show()
+plt.xticks(flights.keys(), labels, rotation = 45)
+key_max = max(flights.keys(), key = (lambda k: flights[k]))
+plt.yticks(np.arange(0, key_max, step = 1))
+#plt.show()
+plt.savefig(__file__+".png", dpi = 800)
