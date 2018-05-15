212ecf8c36f4fb53d502f1a1c14484046c5c0f71
Alejandro Herrada <alejandropons17@gmail.com>
2018-05-14 22:55:35 -0400

    added sendmail script to send emails

--- sendmail.py
+++ sendmail.py
@@ -2,18 +2,18 @@ import sendgrid
 import os
 from sendgrid.helpers.mail import *
 
-def sendemai(recipient, emailSubject, body):
-    sg = sendgrid.SendGridAPIClient(apikey=os.environ.get('SENDGRID_API_KEY'))
+def sendemail(recipient, emailSubject, body):
+    sg = sendgrid.SendGridAPIClient(apikey = os.environ.get('SENDGRID_API_KEY'))
     from_email = Email("app95271153@heroku.com")
     to_email = Email(recipient)
     content = Content("text/plain", body)
     mail = Mail(from_email, emailSubject, to_email, content)
     response = sg.client.mail.send.post(request_body = mail.get())
-    
+
     print("### Email sent to: "+ recipient + " ###")
-	print(response.status_code)
-	print(response.body)
-	print(response.headers)
+    print(response.status_code)
+    print(response.body)
+    print(response.headers)
 
 if __name__ == "__main__":
     sendemail('alejandropons17@gmail.com', "test header", "test body")
